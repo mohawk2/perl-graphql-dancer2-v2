e74ed89e5a3e04d4b64be24ec2d2876d30c05a68
Ed J <mohawk2@users.noreply.github.com>
2017-10-09 23:59:14 +0100

    send valid GraphQL query with JSON vars, show response

--- lib/DemoApp.pm
+++ lib/DemoApp.pm
@@ -7,15 +7,47 @@ use GraphQL::Type::Object;
 use GraphQL::Type::Scalar qw/ $String /;
 use GraphQL::Execution;
 
+set serializer => 'JSON';
+set charset    => 'UTF-8';
+set template   => 'simple';
+
+my $fakedb = {
+    "Africa" => { majorCity => "Lagos" },
+    "Antarctica" => { majorCity => "McMurdo" },
+    "Asia" => { majorCity => "Shanghai" },
+    "Australasia" => { majorCity => "Sydney" },
+    "Europe" => { majorCity => "London" },
+    "North America" => { majorCity => "New York" },
+    "South America" => { majorCity => "Brasilia" },
+};
+
+my $Continent = GraphQL::Type::Object->new(
+    name => 'Continent',
+    fields => {
+        majorCity => {
+            type => $String,
+            resolve => sub {
+                my ( $source ) = @_;
+                return $source->{majorCity};
+            },
+        },
+        qualities => {
+            type => $String,
+            resolve => sub { 'All great' },
+        },
+    },
+);
+
 my $schema = GraphQL::Schema->new(
     query => GraphQL::Type::Object->new(
         name => 'QueryRoot',
         fields => {
             continent => {
-                type => $String,
+                type => $Continent,
+                args => { name => { type => $String } },
                 resolve => sub {
-                    my ( undef, $args ) = @_;
-                    return get_city( $args->{'continent'});
+                    my ( $source, $args ) = @_;
+                    return $source->{$args->{name}};
                 },
             },
         },
@@ -23,7 +55,7 @@ my $schema = GraphQL::Schema->new(
 );
 
 get '/' => sub {
-    template 'index', {
+    send_as html => template 'index', {
         title => 'Perl GraphQL demo app',
     };
 };
@@ -33,16 +65,15 @@ my $JSON = JSON::MaybeXS->new->allow_nonref;
 post '/graphql' => sub {
     return GraphQL::Execution->execute(
         $schema,
-        request->body,
+        body_parameters->{'query'},
+        $fakedb,
         undef,
-        undef,
+        body_parameters->{'variables'},
         undef,
         undef,
     );
 };
 
-sub get_city { return 'unimplemented' };
-
-1; # return true
+1;
 
 __END__
